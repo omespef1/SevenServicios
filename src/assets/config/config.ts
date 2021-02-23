9b95c537d5d213b5e533331e9245b92c224507af
omespef1 <omespef1@hotmail.com>
2021-02-23 15:10:48 -0500

    Version 21.0.2.0 Tiendas

--- src/assets/config/config.ts
+++ src/assets/config/config.ts
@@ -1,6 +1,6 @@
 export const centralizacionUrl :string= "https://sac.digitalware.co:442/Seven_Crm/Centralizacion/api/";
-export const appVersion:string = '20.0.9.0';
-export const appCopyright = 'SEVEN-ERP © 1992-2019';
+export const appVersion:string = '21.0.2.0';
+export const appCopyright = 'SEVEN-ERP © 1992-2021';
 export const appGooglePlayUrl = "";
 export const appAppStoreUrl = "";
 export const developer = 'DigitalWare Team';
@@ -9,4 +9,4 @@ export const developerFacebook = 'https://www.facebook.com/digitalware/';
 export const developerTwitter = 'https://twitter.com/Digital_Ware';
 export const developerWeb = 'http://www.digitalware.com.co/';
 export const developerMode = true;
-export const developerUrl="http://cerezo/REST/SQL/RSEVSERV/api/"
+export const developerUrl="http://argusprueba.comfenalco.com.co/Apps/SevenERPServi/api/"
